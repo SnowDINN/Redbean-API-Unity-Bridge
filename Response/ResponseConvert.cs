516fcfd9dc799c3756b776c9c60b9dab1a24f10f
SEUNGHO LEE <mfactory86@naver.com>
2024-06-07 14:11:26 +0900

    Add .gitignore and ResponseConvert class

---
+++ Response/ResponseConvert.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace BoongGod.Api;
+
+public class ResponseConvert
+{
+	public static string ToResult<T>(T value, int code = 0)
+	{
+		var result = new ResponseResult<T>
+		{
+			StatusCode = code,
+			Result = value
+		};
+
+		return JsonConvert.SerializeObject(result, Formatting.Indented);
+	}
+}
+
+public class ResponseResult<T>
+{
+	public int StatusCode;
+	public T Result;
+}
