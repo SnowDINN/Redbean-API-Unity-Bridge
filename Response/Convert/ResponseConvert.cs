e6e7d6220e87c9d0c8da683a16413d084d5875dc
SEUNGHO LEE <mfactory86@naver.com>
2024-06-07 17:14:06 +0900

    Refactor API response handling and add new metadata files

--- Response/Convert/ResponseConvert.cs
+++ Response/Convert/ResponseConvert.cs
@@ -1,28 +1,29 @@
 ﻿using Newtonsoft.Json;
 
-namespace Redbean.Api;
-
-public class ResponseConvert
+namespace Redbean.Api
 {
-	public static string ToJson<T>(T value, int code = 0)
+	public class ResponseConvert
 	{
-		var result = new ResponseResult<T>
+		public static string ToJson<T>(T value, int code = 0)
 		{
-			StatusCode = code,
-			Result = value
-		};
+			var result = new ResponseResult<T>
+			{
+				StatusCode = code,
+				Result = value
+			};
 
-		return JsonConvert.SerializeObject(result, Formatting.Indented);
+			return JsonConvert.SerializeObject(result, Formatting.Indented);
+		}
+
+		public static ResponseResult<T> ToClass<T>(string response)
+		{
+			return JsonConvert.DeserializeObject<ResponseResult<T>>(response);
+		}
 	}
 
-	public static ResponseResult<T> ToClass<T>(string response)
+	public class ResponseResult<T>
 	{
-		return JsonConvert.DeserializeObject<ResponseResult<T>>(response);
+		public int StatusCode;
+		public T Result;
 	}
-}
-
-public class ResponseResult<T>
-{
-	public int StatusCode;
-	public T Result;
 }
--- Response/UserResponse.cs
+++ Response/UserResponse.cs
@@ -1,18 +1,19 @@
-﻿namespace Redbean.Api;
-
-public class UserResponse
+﻿namespace Redbean.Api
 {
-	public UserInfoModel information;
-	public UserSocialModel social;
-}
+	public class UserResponse
+	{
+		public UserInfoModel information;
+		public UserSocialModel social;
+	}
 
-public class UserInfoModel
-{
-	public string nickname;
-}
+	public class UserInfoModel
+	{
+		public string nickname;
+	}
 
-public class UserSocialModel
-{
-	public string id;
-	public string platform;
+	public class UserSocialModel
+	{
+		public string id;
+		public string platform;
+	}	
 }
