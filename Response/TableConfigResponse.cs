e28d929a6e9d4f0e7ec28ff2d20855aaa9a8edc6
Dinn <dinn@richalien.net>
2024-06-14 15:31:49 +0900

    Clean up folder structure, add table response

---
+++ Response/TableConfigResponse.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Redbean.Api;
+
+namespace Redbean.MVP.Content
+{
+	public class TableConfigResponse : IResponse
+	{
+		[JsonProperty("client")]
+		public TableClientConfig Client { get; set; } = new();
+		
+		[JsonProperty("sheet")]
+		public TableSheetConfig Sheet { get; set; } = new();
+	}
+	
+	public class TableClientConfig
+	{
+		[JsonProperty("id")]
+		public string Id { get; set; } = string.Empty;
+		
+		[JsonProperty("secret")]
+		public string Secret { get; set; } = string.Empty;
+	}
+	
+	public class TableSheetConfig
+	{
+		[JsonProperty("id")]
+		public string Id { get; set; } = string.Empty;
+	}
+}
