5fa5504d4d2ccf854a70a8dee1a0a6202c282b02
SEUNGHO LEE <mfactory86@naver.com>
2024-06-15 01:37:54 +0900

    Create a Returnable Static Instance

--- Response/Extension/ResponseExtension.cs
+++ Response/Extension/ResponseExtension.cs
@@ -9,15 +9,19 @@ namespace Redbean.Api
 	
 	public class Response
 	{
-		public Response(int code, object value)
-		{
-			Code = code;
-			Value = value;
-		}
+		private static readonly Response Default = new();
 		
 		public int Code { get; set; }
 		public object Value { get; set; }
 
+		public static Response Return(int code, object value)
+		{
+			Default.Code = code;
+			Default.Value = value;
+
+			return Default;
+		}
+		
 		public T ToConvert<T>()
 		{
 			if (Value is string result)
@@ -29,12 +33,12 @@ namespace Redbean.Api
 	
 	public static class ResponseExtension
 	{
-		public static Response ToResponse(this string message, ApiErrorType type = 0) => new((int)type, message);
+		public static Response ToResponse(this string message, ApiErrorType type = 0) => Response.Return((int)type, message);
 
-		public static Response ToResponse<T>(this List<T> message, ApiErrorType type = 0) => new((int)type, message);
+		public static Response ToResponse<T>(this List<T> message, ApiErrorType type = 0) => Response.Return((int)type, message);
 
-		public static Response ToResponse(this IDictionary<string, object> snapshot, ApiErrorType type = 0) => new((int)type, snapshot);
+		public static Response ToResponse(this IDictionary<string, object> snapshot, ApiErrorType type = 0) => Response.Return((int)type, snapshot);
 
-		public static Response ToResponse<T>(this T value, ApiErrorType type = 0) where T : IResponse => new((int)type, value);
+		public static Response ToResponse<T>(this T value, ApiErrorType type = 0) where T : IResponse => Response.Return((int)type, value);
 	}
 }
