b76694e2c4ec7251f47232d86c01d0ab95433b65
Dinn <dinn@richalien.net>
2024-06-19 17:21:01 +0900

    Add refresh tokens and automatically request access tokens

--- Response/AccessTokenResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Redbean.Api
-{
-	public class AccessTokenResponse : IResponse
-	{
-		public long Expires { get; set; } = 0;
-		public string AccessToken { get; set; } = "";
-	}	
-}
---
+++ Response/TokenResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Redbean.Api
+{
+	public class TokenResponse : IResponse
+	{
+		public DateTime Expires { get; set; } = DateTime.UtcNow;
+		public string AccessToken { get; set; } = "";
+		public string RefreshToken { get; set; } = "";
+	}	
+}
