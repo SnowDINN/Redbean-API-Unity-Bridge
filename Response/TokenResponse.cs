0ace007c48f4b337f2a77c2c6e2f079bd92efcd1
Dinn <dinn@richalien.net>
2024-06-25 14:39:25 +0900

    Access Token 만료 시간 오차 수정

--- Response/TokenResponse.cs
+++ Response/TokenResponse.cs
@@ -4,9 +4,9 @@ namespace Redbean.Api
 {
 	public class TokenResponse : IResponse
 	{
-		public DateTime AccessTokenExpire = DateTime.UtcNow;
-		public DateTime RefreshTokenExpire = DateTime.UtcNow;
 		public string AccessToken = "";
 		public string RefreshToken = "";
+		public DateTime AccessTokenExpire = DateTime.UtcNow;
+		public DateTime RefreshTokenExpire = DateTime.UtcNow;
 	}	
 }
