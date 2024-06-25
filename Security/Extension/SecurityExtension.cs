5d0939314be656d3475a100d405d70d5cd387cb8
Dinn <dinn@richalien.net>
2024-06-25 14:45:35 +0900

    암호화 확장 클래스 함수명 수정

--- Security/Extension/SecurityExtension.cs
+++ Security/Extension/SecurityExtension.cs
@@ -3,7 +3,7 @@
 	public static class SecurityExtension
 	{
 		private static readonly AES128 AES128 = new();
-		public static string Encrypt(this string value) => AES128.Encryption(value);
-		public static string Decrypt(this string value) => AES128.Decryption(value);
+		public static string Encryption(this string value) => AES128.Encryption(value);
+		public static string Decryption(this string value) => AES128.Decryption(value);
 	}
 }
