0fa9f1986cb01f8a7748c8f8fb26b6d643bd8e28
SEUNGHO LEE <mfactory86@naver.com>
2024-06-30 15:16:24 +0900

    API Post 파일 전송 파라미터 변경

---
+++ Request/AppUploadFilesRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Redbean.Api;
+
+public class AppUploadFilesRequest : Request
+{
+	public MobileType Type = MobileType.None;
+	public RequestFile[] Files { get; set; }
+}
+
+public class RequestFile
+{
+	public string FileName;
+	public byte[] FileData;
+}
