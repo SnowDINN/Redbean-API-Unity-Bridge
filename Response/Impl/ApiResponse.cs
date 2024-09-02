5022afbe95f861ead6af3067288c2dc437bdb5b6
Dinn <dinn@richalien.net>
2024-09-02 15:59:14 +0900

    API Response 데이터 테스트

--- Response/Impl/ApiResponse.cs
+++ Response/Impl/ApiResponse.cs
@@ -1,23 +1,31 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Redbean.Api
 {
 	public class ApiResponse : IApiResponse
 	{
-		[JsonIgnore]
-		public static ApiResponse Default { get; set; } = new();
-		
 		[JsonProperty("errorCode")]
 		public int ErrorCode { get; set; } = -1;
 
 		[JsonProperty("response")]
-		public JObject RawResponse { get; set; } = new();
+		public object RawResponse { get; set; } = new();
 	}
 	
 	public class ApiResponse<T> : ApiResponse
 	{
 		[JsonIgnore]
-		public T Response => RawResponse.Value<T>();
+		private T response;
+
+		[JsonIgnore]
+		public T Response
+		{
+			get
+			{
+				if (response is null)
+					response = JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(RawResponse));
+
+				return response;
+			}
+		}
 	}
 }
